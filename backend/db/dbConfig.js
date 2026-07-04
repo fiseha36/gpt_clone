6d9b5c5a604c0b278a878ad8de6a1a3ff6fde83a
fiseha36 <fisehad5@gmail.com>
2026-07-04 10:34:45 +0100

    updated

--- backend/db/dbConfig.js
+++ backend/db/dbConfig.js
@@ -18,17 +18,30 @@
 // // 2. Export the pool
 // export default db;
 
-import "dotenv/config";
-import mysql from "mysql2/promise";
-
-const db = mysql.createPool({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+
+  ssl: {
+    minVersion: "TLSv1.2",
+    rejectUnauthorized: true,
+  },
 });
 
-export default db;
+// import "dotenv/config";
+// import mysql from "mysql2/promise";
+
+// const db = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE,
+// });
+
+// export default db;
 
 // import "dotenv/config";
 // import mysql from "mysql2/promise";
